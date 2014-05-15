ef1df83c8fc06cdf58c73c6007db9e11c5b6bdd8
MarcObvious <cat166@gmail.com>
2014-05-15 10:53:48 +0000

    Location provider, empty class

---
+++ src/take/me/home/LocationProvicer.java
@@ -0,0 +1,11 @@
+package take.me.home;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.SystemClock;
+
+public class LocationProvider {
+
+
+}
