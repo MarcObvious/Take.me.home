d8a39774221fa47567c23f9afb4f01df940894d5
MarcObvious <cat166@gmail.com>
2014-06-04 12:25:01 +0200

    location changes

--- src/take/me/home/SearchFragment.java
+++ src/take/me/home/SearchFragment.java
@@ -18,6 +18,7 @@ public class SearchFragment extends Fragment implements OnClickListener {
 
 	private ViewGroup mSearchView;
 	private final static String LOG_TAG = "SEARCH FRAGMENT";
+	private Location loc;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,8 +31,10 @@ public class SearchFragment extends Fragment implements OnClickListener {
 		mSearchView = (ViewGroup)inflater.inflate(R.layout.fragment_search, container, false);
 		Button b_home = (Button) mSearchView.findViewById(R.id.Button_home);
 		b_home.setOnClickListener(this);
+		
 		Button b_go_home = (Button) mSearchView.findViewById(R.id.Button_go_home);
 		b_go_home .setOnClickListener(this); 
+		
 		Button b_search = (Button) mSearchView.findViewById(R.id.Button_search);
 		b_search.setOnClickListener(this); 
 
@@ -49,6 +52,7 @@ public class SearchFragment extends Fragment implements OnClickListener {
 					Toast.LENGTH_LONG).show();
 			break;
 		case R.id.Button_home:
+			getLocationClicked();
 			Toast.makeText(getActivity().getApplicationContext(), "THIS IS HOME",
 					Toast.LENGTH_LONG).show();
 			break;
@@ -64,54 +68,60 @@ public class SearchFragment extends Fragment implements OnClickListener {
 
 
 
-	//	public void getLocationClicked(View aView) {
-	//		// Acquire a reference to the system Location Manager
-	//		final LocationManager locationManager = (LocationManager) this
-	//				.getSystemService(Context.LOCATION_SERVICE);
-	//
-	//		// Define a listener that responds to location updates
-	//		LocationListener locationListener = new LocationListener() {
-	//			public void onLocationChanged(Location location) {
-	//				// Called when a new location is found by the network location
-	//				// provider.
-	//
-	//				Toast.makeText(getApplicationContext(),
-	//						"New Location obtained.", Toast.LENGTH_LONG).show();
-	//				makeUseOfNewLocation(location);
-	//				locationManager.removeUpdates(this);
-	//
-	//			}
-	//
-	//			public void onStatusChanged(String provider, int status,
-	//					Bundle extras) {
-	//			}
-	//
-	//			public void onProviderEnabled(String provider) {
-	//			}
-	//
-	//			public void onProviderDisabled(String provider) {
-	//			}
-	//		};
-	//
-	//		// Register the listener with the Location Manager to receive location
-	//		// updates
-	//		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-	//			Log.d(LOG_TAG, "locationManager.isProviderEnabled = true/gps");
-	//			locationManager.requestLocationUpdates(
-	//					LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-	//			Location location = locationManager
-	//					.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	//			if (location != null) {
-	//				makeUseOfNewLocation(location);
-	//			} else {
-	//				Toast.makeText(getApplicationContext(),
-	//						"GPS has yet to calculate location.", Toast.LENGTH_LONG)
-	//						.show();
-	//			}
-	//
-	//		} else {
-	//			Toast.makeText(getApplicationContext(), "GPS is not enabled.",
-	//					Toast.LENGTH_LONG).show();
-	//		}
-	//	}
+		public void getLocationClicked() {
+			// Acquire a reference to the system Location Manager
+			final LocationManager locationManager = (LocationManager) this.getActivity()
+					.getSystemService(Context.LOCATION_SERVICE);
+	
+			// Define a listener that responds to location updates
+			LocationListener locationListener = new LocationListener() {
+				public void onLocationChanged(Location location) {
+					// Called when a new location is found by the network location
+					// provider.
+	
+					Toast.makeText(getActivity().getApplicationContext(),
+							"New Location obtained.", Toast.LENGTH_LONG).show();
+					
+					makeUseOfNewLocation(location);
+					locationManager.removeUpdates(this);
+	
+				}
+	
+				public void onStatusChanged(String provider, int status,
+						Bundle extras) {
+				}
+	
+				public void onProviderEnabled(String provider) {
+				}
+	
+				public void onProviderDisabled(String provider) {
+				}
+			};
+	
+			// Register the listener with the Location Manager to receive location
+			// updates
+			if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+				Log.d(LOG_TAG, "locationManager.isProviderEnabled = true/gps");
+				locationManager.requestLocationUpdates(
+						LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+				Location location = locationManager
+						.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+				if (location != null) {
+					makeUseOfNewLocation(location);
+				} else {
+					Toast.makeText(getActivity().getApplicationContext(),
+							"GPS has yet to calculate location.", Toast.LENGTH_LONG)
+							.show();
+				}
+	
+			} else {
+				Toast.makeText(getActivity().getApplicationContext(), "GPS is not enabled.",
+						Toast.LENGTH_LONG).show();
+			}
+		}
+
+		private void makeUseOfNewLocation(Location location) {
+			loc = location;
+			
+		}
 }
