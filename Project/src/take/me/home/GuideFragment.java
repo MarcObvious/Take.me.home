edb79c78d244dc50d04376242c25a0594a82aec1
MarcObvious <cat166@gmail.com>
2014-06-04 20:17:48 +0200

    new layout

--- Project/src/take/me/home/GuideFragment.java
+++ Project/src/take/me/home/GuideFragment.java
@@ -1,7 +1,6 @@
 package take.me.home;
 
 import android.app.Fragment;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +8,21 @@ import android.view.ViewGroup;
 
 
 public class GuideFragment  extends Fragment{
+	private ViewGroup mSearchView;
+	private final static String LOG_TAG = "GUIDE FRAGMENT";
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 	}
 
-//	@Override
-//	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//		//mSearchView = (ViewGroup)inflater.inflate(R.layout.fragment_search, container, false);
-//		GLSurfaceView view = new GLSurfaceView(this.getActivity());
-//	    view.setRenderer(new OpenGLRenderer());
-//	    this.getActivity().setContentView(view);
-//	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		mSearchView = (ViewGroup)inflater.inflate(R.layout.fragment_guide, container, false);
+
+
+		return mSearchView;
+
+	}
 }
