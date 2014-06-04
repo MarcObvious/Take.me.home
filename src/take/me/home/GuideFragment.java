4e81d3015d9dfb9e0b7de330ccd149bf416b9d6b
MarcObvious <cat166@gmail.com>
2014-06-04 17:09:02 +0200

    new fragment, with opengl

---
+++ src/take/me/home/GuideFragment.java
@@ -0,0 +1,22 @@
+package take.me.home;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GuideFragment  extends Fragment{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		//mSearchView = (ViewGroup)inflater.inflate(R.layout.fragment_search, container, false);
+		return View;
+	}
+}
