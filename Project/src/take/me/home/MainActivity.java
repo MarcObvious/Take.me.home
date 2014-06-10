f53c1b225ca93c579bd4cea7c9eaa1f2372b29ca
Giacomo Tomolillo <giacomotomolillo@ccpn-40-065.eduroam.upf.edu>
2014-06-10 20:54:46 +0200

    Google location

--- Project/src/take/me/home/MainActivity.java
+++ Project/src/take/me/home/MainActivity.java
@@ -1,17 +1,18 @@
 package take.me.home;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,16 +21,33 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
 public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks, GooglePlayServicesClient.ConnectionCallbacks,
-GooglePlayServicesClient.OnConnectionFailedListener {
+GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
 	
 	private SearchFragment mSearchFragment;
 	private SettingsFragment mSettingsFragment;
 	private GuideFragment mGuideFragment;
 	private MapsFragment mMapsFragment;
 	
-	 private final static int
-     CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	private LocationClient mLocationClient;
+	private Location mLocation;
+	private LocationRequest mLocationRequest;
+	
+	private final List<LocationUpdatesListener> listeners = new ArrayList<LocationUpdatesListener>();
+	
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
 	
 
     /**
@@ -46,6 +64,12 @@ GooglePlayServicesClient.OnConnectionFailedListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mLocationClient = new LocationClient(this, this, this);
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -55,9 +79,35 @@ GooglePlayServicesClient.OnConnectionFailedListener {
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
-        
-        
     }
+    
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	mLocationClient.connect();
+    }
+    
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	if (mLocationClient.isConnected())
+            mLocationClient.removeLocationUpdates(this);
+    	mLocationClient.disconnect();
+    }
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		switch (requestCode) {
+        case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                	mLocationClient.connect();
+                break;
+            }
+		}
+	}
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
@@ -82,6 +132,9 @@ GooglePlayServicesClient.OnConnectionFailedListener {
     			break;
     		default: fragment = PlaceholderFragment.newInstance(position + 1);
     	}
+    	
+    	if (LocationUpdatesListener.class.isAssignableFrom(fragment.getClass()) && !listeners.contains(fragment))
+    		listeners.add((LocationUpdatesListener)fragment);
     	
 		getFragmentManager().beginTransaction()
             .replace(R.id.container, fragment)
@@ -203,60 +256,33 @@ GooglePlayServicesClient.OnConnectionFailedListener {
     }
 
 	@Override
-	public void onConnectionFailed(ConnectionResult arg0) {
-//		// TODO Auto-generated method stub
-//		 if (connectionResult.hasResolution()) {
-//	            try {
-//	                // Start an Activity that tries to resolve the error
-//	                connectionResult.startResolutionForResult(
-//	                        this,
-//	                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
-//	                /*
-//	                 * Thrown if Google Play services canceled the original
-//	                 * PendingIntent
-//	                 */
-//	            } catch (IntentSender.SendIntentException e) {
-//	                // Log the error
-//	                e.printStackTrace();
-//	            }
-//	        } else {
-//	            /*
-//	             * If no resolution is available, display a dialog to the
-//	             * user with the error.
-//	             */
-//	            showErrorDialog(connectionResult.getErrorCode());
-//	        }
-		
-	}
-
-	@Override
-	public void onConnected(Bundle arg0) {
-		 Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-		
-	}
-
-	@Override
-	public void onDisconnected() {
-		 Toast.makeText(this, "Disconnected. Please re-connect.",
-	                Toast.LENGTH_SHORT).show();
-		
-	}
-	private boolean servicesConnected() {
-        // Check that Google Play services is available
-        int resultCode =
-                GooglePlayServicesUtil.
-                        isGooglePlayServicesAvailable(this);
-        // If Google Play services is available
-        if (ConnectionResult.SUCCESS == resultCode) {
-            // In debug mode, log the status
-            Log.d("Location Updates",
-                    "Google Play services is available.");
-            // Continue
-            return true;
-        // Google Play services was not available for some reason
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
         } else {
-            // Get the error code
-            int errorCode = connectionResult.getErrorCode();
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+        	int errorCode = connectionResult.getErrorCode();
             // Get the error dialog from Google Play services
             Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
                     errorCode,
@@ -266,14 +292,34 @@ GooglePlayServicesClient.OnConnectionFailedListener {
             // If Google Play services can provide an error dialog
             if (errorDialog != null) {
                 // Create a new DialogFragment for the error dialog
-                ErrorDialogFragment errorFragment =
-                        new ErrorDialogFragment();
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
                 // Set the dialog in the DialogFragment
                 errorFragment.setDialog(errorDialog);
                 // Show the error dialog in the DialogFragment
-                errorFragment.show(getSupportFragmentManager(),
-                        "Location Updates");
+                errorFragment.show(getFragmentManager(), "Location Updates");
             }
         }
     }
+
+	@Override
+	public void onConnected(Bundle bundle) {
+		 Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+	     mLocationClient.requestLocationUpdates(mLocationRequest, this);
+	}
+
+	@Override
+	public void onDisconnected() {
+		 Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		updateLocation(location);
+	}
+	
+	private void updateLocation(Location location) {
+		mLocation = location;
+		for (LocationUpdatesListener listener : listeners)
+			listener.onLocationChanged(mLocation);
+	}
 }
--- Project/src/take/me/home/SettingsFragment.java
+++ Project/src/take/me/home/SettingsFragment.java
@@ -1,13 +1,9 @@
 package take.me.home;
 
-import java.util.Random;
-
 import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,24 +14,20 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SettingsFragment extends Fragment implements OnClickListener {
+public class SettingsFragment extends Fragment implements OnClickListener, LocationUpdatesListener {
 
 	private ViewGroup mSettingsView;
 	private final static String LOG_TAG = "SEARCH FRAGMENT";
 	private float default_value = 0;
-	private Location loc;
+	private Location homeLocation, currentLocation;
 
+	private boolean saveOnNextUpdate = false;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		if (!getLocation()) {
-			loc = new Location("");
-
-			loc.setLatitude(0.0);
-			loc.setLongitude(0.0);		
-		}
 	}
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		mSettingsView = (ViewGroup)inflater.inflate(R.layout.fragment_settings, container, false);
@@ -46,10 +38,10 @@ public class SettingsFragment extends Fragment implements OnClickListener {
 		Button b_fake = (Button) mSettingsView.findViewById(R.id.Button_fake_home);
 		b_fake.setOnClickListener(this);
 
-		drawLocation(loc);
+		if (getLocation())
+			drawLocation(homeLocation);
 
 		return mSettingsView;
-
 	}
 
 	private boolean getLocation () {
@@ -60,25 +52,28 @@ public class SettingsFragment extends Fragment implements OnClickListener {
 
 		if (lat == default_value && lon == default_value) return false;
 
-		loc = new Location("");
+		homeLocation = new Location("");
 
-		loc.setLatitude((double) lat);
-		loc.setLongitude((double) lon);
+		homeLocation.setLatitude((double) lat);
+		homeLocation.setLongitude((double) lon);
 
 		return true;
 
 	}
 
 	public void saveLocation() {
-		SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-
-		SharedPreferences.Editor editor = sharedPref.edit();
-
-		editor.putFloat("lat_home", (float) loc.getLatitude());
-
-		editor.putFloat("lon_home", (float) loc.getLongitude());
-
-		editor.commit();
+		if (currentLocation == null) {
+			saveOnNextUpdate = true;
+			return;
+		}
+		
+		getActivity().getPreferences(Context.MODE_PRIVATE)
+			.edit()
+			.putFloat("lat_home", (float) currentLocation.getLatitude())
+			.putFloat("lon_home", (float) currentLocation.getLongitude())
+			.commit();
+		
+		saveOnNextUpdate = false;
 	}
 
 	@Override
@@ -90,103 +85,20 @@ public class SettingsFragment extends Fragment implements OnClickListener {
 			Toast.makeText(getActivity().getApplicationContext(), "THIS IS HOME",
 					Toast.LENGTH_LONG).show();
 			break;
-		case R.id.Button_fake_home:
-			getFakeLocation();
-			saveLocation();
-			Toast.makeText(getActivity().getApplicationContext(), "THIS IS FAKE HOME",
-					Toast.LENGTH_LONG).show();
-			break;
 		default:
 			Log.i(LOG_TAG, "Unknown: " + view.getId());
 			break;
 		}
 	}
 
-	public void getFakeLocation() {
-
-		Random r = new Random();
-
-		Double _lat = r.nextDouble() *100;
-		Double _lon = r.nextDouble() *100;
-
-
-		loc = new Location("");
-		loc.setLatitude(/*41.3869691*/_lat);
-		loc.setLongitude(/*2.1706107*/_lon);
-
-		drawLocation(loc);	
-	}
-
 	public void getLocationClicked() {
-		// Acquire a reference to the system Location Manager
-				
-		final LocationManager locationManager = (LocationManager) getActivity()
-				.getSystemService(Context.LOCATION_SERVICE);
-
-		// Define a listener that responds to location updates
-		LocationListener locationListener = new LocationListener() {
-			public void onLocationChanged(Location location) {
-				// Called when a new location is found by the network location
-				// provider.
-
-				Toast.makeText(getActivity().getApplicationContext(),
-						"New Location obtained.", Toast.LENGTH_LONG).show();
-
-				makeUseOfNewLocation(location);
-				locationManager.removeUpdates(this);
-
-			}
-
-			public void onStatusChanged(String provider, int status,
-					Bundle extras) {
-			}
-
-			public void onProviderEnabled(String provider) {
-			}
-
-			public void onProviderDisabled(String provider) {
-			}
-		};
-
-		// Register the listener with the Location Manager to receive location
-		// updates
-		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-			Log.d(LOG_TAG, "locationManager.isProviderEnabled = true/gps");
-			locationManager.requestLocationUpdates(
-					LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-			Location location = locationManager
-					.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-			if (location != null) {
-				makeUseOfNewLocation(location);
-			} else {
-				Toast.makeText(getActivity().getApplicationContext(),
-						"GPS has yet to calculate location.", Toast.LENGTH_LONG)
-						.show();
-			}
-
-		} else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-			Log.d(LOG_TAG, "locationManager.isProviderEnabled = true/network");
-			locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, locationListener, null);
-			
-			//locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-			Location location = locationManager
-					.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-			if (location != null) {
-				makeUseOfNewLocation(location);
-			} else {
-				Toast.makeText(getActivity().getApplicationContext(),
-						"Network has yet to calculate location.", Toast.LENGTH_LONG)
-						.show();
-			}
-		}
-		else {
-			Toast.makeText(getActivity().getApplicationContext(), "GPS is not enabled, and Network Position disponible",
-					Toast.LENGTH_LONG).show();
-		}
+		saveLocation();
 	}
 
 	private void drawLocation(Location loc) {
 
+		if (loc == null) return;
+		
 		TextView lat = (TextView) mSettingsView.findViewById(R.id.latitude);
 
 		lat.setText( Double.toString(loc.getLatitude()) );	
@@ -198,11 +110,10 @@ public class SettingsFragment extends Fragment implements OnClickListener {
 
 
 	}
-
-	private void makeUseOfNewLocation(Location location) {
-		loc = location;
-
-		drawLocation(loc);
-
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		currentLocation = location;
+		if (saveOnNextUpdate) saveLocation();
 	}
 }
